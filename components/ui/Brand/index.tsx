b55eb66e28bd5193515f21de27723561766abe34
VincenzoManto <vincmanto@gmail.com>
2025-03-10 17:58:20 +0100

    Update brand component to use heading for improved accessibility and styling

--- components/ui/Brand/index.tsx
+++ components/ui/Brand/index.tsx
@@ -5,7 +5,7 @@ export default () => (
   <>
     <div className="flex items-center gap-2 text-white">
       <Image src={logoDark} alt="TeraBrain logo" width={50} className="m-2" />
-      Terabrain
+      <h3 className="text-xl">TeraBrain</h3>
     </div>
   </>
 );
